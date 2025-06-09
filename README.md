9d2c33bd953951010c558cdf32a2c56b8645bf6e
Dmitrii Kustov <github@simbiat.dev>
2025-06-09 16:22:24 +0300

    Installer class
    Signed-off-by: Dmitrii Kustov <github@simbiat.dev>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# db-installer
-Small library to help with installation of database stuff for libraries needing that
+# DB Installer
+
+Small library to install or update database dependencies of a library, based on its current version.
+
+```php
+new \Simbiat\Database\Installer($dbh)::install(string $pattern, string $version = '0.0.0')
+```
+
+The constructor of the `Installer` class requires a `\PDO` object to establish connection or it can be or `null` if you are using [DB Pool](https://github.com/Simbiat/db-pool) library.
+
+Then pass a pattern for `glob()` to get the files with SQL commands you need to run and current `version` string. The function will scan respective folder(s) for files and then compare their names to the version provided. Those files that have a "greater" version number (based on `version_compare()`) will be concatenated and then run against the database. If the commands succeed, the function will return `true`, otherwise an exception will be thrown.
---
+++ src/Installer.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types = 1);
+
+namespace Simbiat\Database;
+
+/**
+ * Install database dependencies for a library
+ */
+class Installer
+{
+    /**
+     * @var null|\PDO PDO object to run queries against
+     */
+    public static ?\PDO $dbh = null;
+    
+    /**
+     * @param \PDO|null $dbh PDO obj
+     */
+    public function __construct(?\PDO $dbh = null)
+    {
+        if ($dbh === null) {
+            if (method_exists(Pool::class, 'openConnection')) {
+                self::$dbh = Pool::openConnection();
+            } else {
+                throw new \RuntimeException('Pool class not loaded and no PDO object provided.');
+            }
+        } else {
+            self::$dbh = $dbh;
+        }
+    }
+    
+    /**
+     * Install database dependencies for a library based on the current version
+     *
+     * @param string $pattern Path and file pattern for GLOB
+     * @param string $version Version
+     *
+     * @return bool
+     */
+    public static function install(string $pattern, string $version = '0.0.0'): bool
+    {
+        #Generate SQL to run
+        $sql = '';
+        #Get SQL from all files. Sorting is required since we need a specific order of execution.Add commentMore actions
+        /** @noinspection LowPerformingFilesystemOperationsInspection */
+        foreach (glob($pattern) as $file) {
+            #Compare version and take only newer ones
+            if (version_compare(basename($file, '.sql'), $version, 'gt')) {
+                #Get contents from the SQL file
+                $sql .= file_get_contents($file);
+            }
+        }
+        #If empty - we are up to date
+        if (empty($sql)) {
+            return true;
+        }
+        return Query::query($sql);
+    }
+}
